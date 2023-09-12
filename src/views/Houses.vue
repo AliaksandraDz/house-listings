92068aaf94cc7aeb45ee58a23d80da62807f1de5
Alexandra <wilderweinundich@gmail.com>
2023-09-12 19:30:42 +0300

    Add a search button in the input

--- src/views/Houses.vue
+++ src/views/Houses.vue
@@ -5,17 +5,25 @@
       <button class="create-button">+ Create new</button>
     </div>
     <div class="container">
-      <input type="text" class="input-field" placeholder="Search for a house">
+      <div class="input-field-container">
+        <input type="text" class="input-field" v-model="searchInput" placeholder="Search for a house" @keydown.enter="performSearch">
+        <button class="search-button" @click="performSearch"> <!-- This button will trigger the search -->
+          <img src="../assets/ic_search@3x.png" alt="Search" />
+        </button>
+      </div>
       <div class="btn-group">
           <button class="button-right" :class="{ active: isActive === 'size' }" @click="toggleActive('size')">Size</button>
           <button class="button-left" :class="{ active: isActive === 'price' }" @click="toggleActive('price')">Price</button>
       </div>
     </div>
     <div class="house-listing">
-      <div v-for="house in sortedHouses" :key="house">
+      <div v-for="house in filteredHouses" :key="house">
         <HouseListing :house="house" /> <!--passing a house as a prop-->
       </div>
     </div>
+  </div>
+  <div class="search-result">
+    <p>{{ filteredHouses.length }} houses found</p>
   </div>
 </template>
 
@@ -30,29 +38,47 @@ export default {
     HouseListing
   },
   setup() {
-    const isActive = ref('size');
+    const searchInput = ref('');
+    const searchQuery = ref('');
+    const isActive = ref('price');
 
     const houses = [
       { title: "Stokvisstraat 132", price: 500, address: "1011AA Amsterdam", size: 120 },
-      { title: "Stokvisstraat 132", price: 1000, address: "1011AA Rotterdam", size: 80 },
-      { title: "Stokvisstraat 132", price: 300, address: "1011AA Harlem", size: 30 },
+      { title: "Gokvisstraat 50", price: 1000, address: "2022AA Rotterdam", size: 80 },
+      { title: "Zokvisstraat 7", price: 300, address: "3033AA Haarlem", size: 30 },
     ];
 
-    const sortedHouses = computed(() => {
-      if (isActive.value === 'price') {
-        // Sort houses by price in ascending order
-        return [...houses].sort((a, b) => a.price - b.price);
-      } else if (isActive.value === 'size') {
-        // Sort houses by size in ascending order
-        return [...houses].sort((a, b) => a.size - b.size);
-      }
-    });
+    const filteredHouses = ref([...houses]);
 
     const toggleActive = (buttonType) => {
       isActive.value = buttonType;
+      sortHouses();
     };
 
-    return { houses, isActive, sortedHouses, toggleActive };
-  },
+    const performSearch = () => {
+    searchQuery.value = searchInput.value.toLowerCase().trim(); // Update searchQuery
+    console.log(searchInput.value);
+    filteredHouses.value = houses.filter((house) => {
+      return (
+        house.title.toLowerCase().includes(searchQuery.value) ||
+        house.address.toLowerCase().includes(searchQuery.value)
+      );
+    });
+    sortHouses();
+  };
+
+    const sortHouses = () => {
+      if (isActive.value === 'price') {
+        filteredHouses.value.sort((a, b) => a.price - b.price);
+      } else if (isActive.value === 'size') {
+        filteredHouses.value.sort((a, b) => a.size - b.size);
+      }
+    };
+
+    // Initially, sort the houses
+    sortHouses();
+
+    return { houses, isActive, searchQuery, searchInput, filteredHouses, toggleActive, performSearch };
+    },
 }
 </script>
