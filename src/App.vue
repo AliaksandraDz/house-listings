53433953f07cb22cf581af73f188ee189b3685e6
Alexandra <wilderweinundich@gmail.com>
2023-11-06 20:14:58 +0300

    Change logo and fix error 404

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
       <nav>
-        <img alt="DTT logo" src="./assets/img_logo_dtt@3x.png">
+        <img alt="logo" src="./assets/img_logo@3x.png">
         <router-link to="/">Houses</router-link>
         <router-link to="/about">About</router-link>
       </nav>
