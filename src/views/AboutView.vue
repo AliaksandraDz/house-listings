0b9ae1e8dd54435fbd94b621be21464fad089440
Alexandra <wilderweinundich@gmail.com>
2024-01-25 21:03:50 +0300

    add content to About; add fonts

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,40 +1,28 @@
 <template>
-  <div>
-    <hr>
-    <MqResponsive target="xs">
-      <div>This is an XS Screen</div>
-    </MqResponsive>
-    <MqResponsive target="sm">
-      <div>This is an SM Screen</div>
-    </MqResponsive>
-    <MqResponsive target="md">
-      <div>This is an MD Screen</div>
-    </MqResponsive>
-    <MqResponsive target="lg">
-      <div>This is an LG Screen</div>
-    </MqResponsive>
-    <MqResponsive target="xl">
-      <div>This is an XL Screen</div>
-    </MqResponsive>
-    <MqResponsive target="xxl">
-      <div>This is an XXL Screen</div>
-    </MqResponsive>
-
-    <!-- Multiple -->
-    <MqResponsive :target="['xs', 'lg']">
-      <div>This will only show on x-small or large screens</div>
-    </MqResponsive>
-    <hr>
-  </div>
+    <div>
+        <MqResponsive :target="['xxl', 'xl', 'lg']">
+            <AboutLarge/>
+        </MqResponsive>
+        <MqResponsive :target="['xs', 'sm', 'md']">
+            <AboutSmall/>
+        </MqResponsive>
+    </div>
 </template>
 
 <script>
+import AboutLarge from '@/components/AboutLarge.vue'
+import AboutSmall from '@/components/AboutSmall.vue'
 import { onMounted, defineComponent } from "vue";
 import { MqResponsive } from "vue3-mq";
 import { updateBreakpoints } from "vue3-mq";
+
 export default defineComponent({
   name: 'About',
-  components: { MqResponsive },
+  components: { 
+    AboutLarge,
+    AboutSmall,
+    MqResponsive
+  },
   setup() {
     onMounted(() => {
       updateBreakpoints({
