8ac28c34483508f8c3ad16822b1375cecf3bb460
Alexandra <wilderweinundich@gmail.com>
2024-03-18 17:17:24 +0100

    responsive About

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <h2>About DTT Real Estate</h2>
+    <h1></h1>
+    <h2 class="about-hidden-h2">About DTT Real Estate</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque quis elit ex. Vivamus ut nulla lorem. Mauris metus lorem, condimentum non nulla non, tempus porta libero. Sed in convallis velit. Morbi accumsan volutpat molestie. Pellentesque suscipit dolor purus, eu finibus tellus commodo nec. Vivamus nibh sem, ornare in urna ac, consectetur vehicula lorem.</p>
     <p>Phasellus id interdum metus. Cras sed neque vestibulum, dignissim felis in, imperdiet mi.</p>
     <h2>Design and Development</h2>
-    <div class="about-logo-sm">
+    <div class="about-logo">
         <img src="../assets/img_logo_dtt@3x.png" alt="Logo" />
         <span>By Dtt
             <br />
