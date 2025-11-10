e3819e16ece37fc241a5bb5045b2d9e4cfbb0403
TRANSFER\sasha_Dzekanava <sasha.dzekanava@transfer-solutions.com>
2025-11-10 13:35:59 +0100

    switch to contabo server

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = 'https://dzekanava.com:3333/houses/';
+// export const baseUrl = 'https://dzekanava.com:3333/houses/';
+export const baseUrl = 'https://aliaksandradz.twilightparadox.com/dbHouses.json/';
