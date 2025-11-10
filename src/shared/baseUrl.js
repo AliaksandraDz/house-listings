3456b283d38435ec0505dd3a4074d0dd0d25855b
TRANSFER\sasha_Dzekanava <sasha.dzekanava@transfer-solutions.com>
2025-11-10 14:54:54 +0100

    switch to amazon server

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://dzekanava.com:3333/houses/';
-export const baseUrl = 'https://aliaksandradz.twilightparadox.com/dbHouses.json/';
+export const baseUrl = 'https://dzekanava.com:3333/houses/';
+// export const baseUrl = 'https://aliaksandradz.twilightparadox.com/dbHouses.json/';
